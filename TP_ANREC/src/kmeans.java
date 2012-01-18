8c68a7d2f49619d1710cfa29118e9efdf690e03e
Antoine Richard <ant.richard@gmail.com>
2012-01-18 12:01:08 +0100

    *

--- TP_ANREC/src/kmeans.java
+++ TP_ANREC/src/kmeans.java
@@ -27,12 +27,12 @@ public class kmeans {
 	// Création et remplissage de la matrice M contenant nos données et l'attribution d'un individu à un cluser (dernière colonne)
 	M = matrice(n,p+1)
 	Pour i=0 à n
-		•	Pour j=0 à p
-		•	M(i,j) = X(i,j)
-		•	Fin pour
+		Pour j=0 à p
+			M(i,j) = X(i,j)
+		Fin pour
 	Fin pour
 	Pour i=0 à n
-		•	M(i,p+1)=0
+		M(i,p+1)=0
 	Fin pour
 
 	// matrice des centres de gravité des k clusters
@@ -43,49 +43,49 @@ public class kmeans {
 	// Affecter les n individus aux k clusters :
 
 	Pour i=0 à n-1
-		•	VecteurDistance <-- (0,0)
-		•	distance <-- 0
-		•	OldCluster <-- -1
-		•	
-		•	Pour j=0 à k-1
-		•	// Calcul de la distance euclidienne entre l'individu X(j) et le barycentre du cluster j K(j)
-		•	distance <-- |X(j)-K(j)|
-		•	// On va voir si la nouvelle distance est plus petite que la plus petite de toutes les distances précédentes
-		•	Si j=1 alors
-		•	VecteurDistance(x) = distance
-		•	VecteurDistance(y) = j
-		•	Sinon si ((distance) < VecteurDistance(x)) alors
-		•	VecteurDistance(x) = distance
-		•	VecteurDistance(y) = j
-		•	Fin si
-		•	Fin pour
-		•	
-		•	// Attribuer l'individu à son (nouveau) cluster
-		•	// cad ajouter à la dernière colonne de la matrice M l'indice du cluster
-		•	OldCluster <-- M(i,p+1)
-		•	M(i,p+1) <-- VecteurDistance(y)
-		•	// Regarder si l'indice du cluster a changé
-		•	// Si oui, CheckChange(i) = true | Si non, CheckChange(i) = false
-		•	Boolean CheckChange(i) <-- isDifferent(Oldcluster,VecteurDistance(y))
-		•	
+		VecteurDistance <-- (0,0)
+		distance <-- 0
+		OldCluster <-- -1
+			
+		Pour j=0 à k-1
+			// Calcul de la distance euclidienne entre l'individu X(j) et le barycentre du cluster j K(j)
+			distance <-- |X(j)-K(j)|
+			// On va voir si la nouvelle distance est plus petite que la plus petite de toutes les distances précédentes
+			Si j=1 alors
+				VecteurDistance(x) = distance
+				VecteurDistance(y) = j
+			Sinon si ((distance) < VecteurDistance(x)) alors
+				VecteurDistance(x) = distance
+				VecteurDistance(y) = j
+			Fin si
+		Fin pour
+			
+		// Attribuer l'individu à son (nouveau) cluster
+		// cad ajouter à la dernière colonne de la matrice M l'indice du cluster
+		OldCluster <-- M(i,p+1)
+		M(i,p+1) <-- VecteurDistance(y)
+		// Regarder si l'indice du cluster a changé
+		// Si oui, CheckChange(i) = true | Si non, CheckChange(i) = false
+		Boolean CheckChange(i) <-- isDifferent(Oldcluster,VecteurDistance(y))
+			
 	Fin pour
 
 	// Calcul des k nouveaux centres de gravité
 
 	Pour j=0 à k-1
-		•	Nbindividus <-- 0
-		•	Somme <-- (0,0,...,0) //(1 ligne, p colonnes)
-		•	Pour i=0 à n-1
-		•	Si (M(i,p+1) = j) alors
-		•	Pour l=0 à p-1
-		•	Somme(l) = Somme(l) + M(i,l)
-		•	NbIndividus++;
-		•	Fin pour
-		•	Fin si
-		•	Fin pour
-		•	Pour l=0 à p-1
-		•	K(j,l) <-- Somme(l) / NbIndividus
-		•	Fin pour
+		Nbindividus <-- 0
+		Somme <-- (0,0,...,0) //(1 ligne, p colonnes)
+			Pour i=0 à n-1
+				Si (M(i,p+1) = j) alors
+					Pour l=0 à p-1
+						Somme(l) = Somme(l) + M(i,l)
+						NbIndividus++;
+					Fin pour
+				Fin si
+			Fin pour
+		Pour l=0 à p-1
+			K(j,l) <-- Somme(l) / NbIndividus
+		Fin pour
 	Fin pour
 
 	Fin Tant que
